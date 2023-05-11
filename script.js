543b309fa8a654b9979e9f520369e6b557a98eb9
rahulsinghtensech <111653775+rahulsinghtensech@users.noreply.github.com>
2023-05-11 14:39:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// document.getElementById("h").innerHTML="rahul";
+// document.write("ram");
+let data =[23,25,21,117,45];
+// for(let i=0;i<data.length;i++)
+// {
+//     console.log(`Array elements ${i}:${data[i]} <br>`);
+// }
+// function getElement()
+// {   
+//     let el=document.getElementById(`element`).value;
+//     if(el<data.length && typeof parseInt(el)==="number" && el >= 0)
+//     {
+//         alert(data[el]); 
+//     }
+//     else
+//     {
+//         alert("Please enter valid input");
+//     }
+    
+// }
+// let c=confirm("Please confirm your credentials");
+// document.write(`the data you entered is ${c}`);
+let type = "hello";
+console.log(type);
+alert(typeof type);
+let Newtype = Number(type);
+console.log(Newtype);
+alert(typeof Newtype);
